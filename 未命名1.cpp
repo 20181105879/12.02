5dc0761ff55d81514c9798a3d4393d848fb974ba
20181105879 <2605753145@qq.com>
2019-12-03 18:55:14 +0800

    gdfdgs
    dsgd

--- 未命名1.cpp
+++ 未命名1.cpp
@@ -1,62 +1,88 @@
 #include <iostream>
 using namespace std;
-long long  s[30050];
-long long  ss[1050];
-long long s1[30050];
-int main(int argc, const char * argv[]) {
-    
-   // cout<<"sdg"<<endl;
-    long long  n,m;
-    cin>>n>>m;
-    for(long i=0;i<m;i++)
-    {
-        long long t;
-        cin>>t;
-        for(long long j=0;j<t;j++)
-        {
-            cin>>ss[j];
-            if(j==0)
-            {
-                if(s[ss[j]]==0)
-                {
-                    s[ss[0]]=ss[0];
-                }
-            }
-            else
-            {
-                s[ss[j]]=ss[0];
-                
-            }
+int a[10000000];
+int b[10000000];
+int c[10000000];
+int d[10000000];
+int main()
+{
+	int n,m;
+	cin>>n;
+	int q,w;
+	for(int i=0;i<n;i++)
+	{
+		cin>>q>>w;
+		a[w]+=q;
+	}
+	cin>>m;
+	for(int i=0;i<m;i++)
+	{
+		cin>>q>>w;
+		b[w]+=q;
+	}
+	for(int i=0;i<11000;i++)
+	{
+		
+		if(a[i]!=0)
+		{
+			for(int j=0;j<11000;j++)
+			{
+				if(b[j]!=0)
+				{
+					c[i+j]+=(a[i]*b[j]);
+				}
+			}
+		}
+		
+	}
+	int count=0;
+	for(int i=10005;i>=0;i--)
+	{
+		if(c[i]!=0)
+		{
+			if(count!=0)
+			{
+				cout<<" "<<c[i]<<" "<<i;
+			}
+			else
+			{
+				cout<<c[i]<<" "<<i;
+			}
+			count++;
+		}
+	}
+	if(count==0)
+	{
+		cout<<0<<" "<<0; 
+	}
+	cout<<endl;
+	for(int i=0;i<10005;i++)
+	{
+		if(a[i]!=0||b[i]!=0)
+		{
+			d[i]=a[i]+b[i];
+		}
+	}
+	count=0;
+	for(int i=10005;i>=0;i--)
+	{
+		if(d[i]!=0)
+		{
+			if(count!=0)
+			{
+				cout<<" "<<d[i]<<" "<<i;
+			}
+			else
+			{
+				cout<<d[i]<<" "<<i;
+			}
+			count++;
+		}
+	}
+	if(count==0)
+	{
+		cout<<0<<" "<<0; 
+	}
 
-        }
-    }
-//    for(long long i=1;i<=n;i++)
-//    {
-//        cout<<s[i]<<endl;
-//    }
-    long long max=0;
-    for(long long i=1;i<=n;i++)
-    {
-        s1[s[i]]++;
-        if(s1[s[i]]>max)
-        {
-            max=s1[s[i]];
-        }
-        
-    }
-    if(m==0)
-    {
-        cout<<"1"<<endl;
-        return 0;
-    }
-    cout<<max<<endl;
-    
-    
-    
-    
-    
-    
-    
-    return 0;
+	return 0;
 }
-
